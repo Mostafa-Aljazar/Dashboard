0e56aa3996b980cad01a9456cc37bda604853f11
Mostafa-Aljazar <mostafaibrahim20032020@gmail.com>
2025-01-04 20:04:19 +0200

    feat: create login page using api

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,18 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./auth/login/page";
 import AuthLayout from "./auth/layout";
-import Verifications from "./auth/verification/page";
+import OTP from "./auth/otp/page";
+import Dashboard from "./dashboard/page";
 function Routers() {
   return (
     <Routes>
       <Route path="auth" element={<AuthLayout />}>
         <Route index element={<Navigate to={"/auth/login"} />} />
         <Route path="login" element={<Login />} />
-        <Route path="verify" element={<Verifications />} />
+        {/* <Route path="otp" element={<OTP />} /> */}
       </Route>
       <Route path="/" element={<div className="bg-gray-600">home</div>}></Route>
+      <Route path="/dashboard" element={<Dashboard />}></Route>
     </Routes>
   );
 }
